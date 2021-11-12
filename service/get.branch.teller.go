9cccbbaa5dd26235327c9a69f7f516f66b518493
Achmad Irwan <achmad.irwan@dwidasa.com>
2021-11-12 16:55:06 +0800

    update

---
+++ service/get.branch.teller.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+
+	"gitlab.pactindo.com/ebanking/common/log"
+	"gitlab.pactindo.com/ebanking/common/pg"
+	"gitlab.pactindo.com/ebanking/common/trycatch"
+	wtproto "gitlab.pactindo.com/ebanking/web-teller/proto"
+)
+
+type branchTeller struct {
+	BranchCode string `json:"branch_code"`
+	BranchName string `json:"branch_name"`
+	Address    string `json:"address"`
+}
+
+func (h *WebTellerHandler) GetBranchTeller(_ context.Context, req *wtproto.APIREQ, res *wtproto.APIRES) error {
+	defer func() {
+		log.Infof("[%s] response: %v", req.Headers["Request-ID"], string(res.Response))
+	}()
+	defer trycatch.Catch(func(e trycatch.Exception) {
+		log.Infof("[%s] error : %v", req.Headers["Request-ID"], e)
+		res.Response, _ = json.Marshal(newResponse("99", "Internal Server Error"))
+	})
+
+	sql := "select branch_code,branch_name, address from m_branch_teller"
+	var result []branchTeller
+	rows, err := pg.DB.Query(sql)
+	if err != nil {
+		return nil
+	}
+	log.Infof("Test query:", rows)
+	for rows.Next() {
+		datas := branchTeller{}
+		err := rows.Scan(
+			&datas.BranchCode,
+			&datas.BranchName,
+			&datas.Address,
+		)
+		if err != nil {
+			return nil
+		}
+		result = append(result, datas)
+	}
+	res.Response, _ = json.Marshal(newResponseWithData("00", "Sukses", result))
+	return nil
+}
